4db1f1cdd3974bdca7eb21f1e84960487813072b
Iman Saadat <e.saadat72@gmail.com>
2023-06-04 23:31:18 +0330

    skills was updated

--- src/data/data.js
+++ src/data/data.js
@@ -26,3 +26,153 @@ export const experienceData = [
     icon: <SiFiverr />,
   }
 ];
+
+// Skill Data
+
+export const skilldata = [
+    {
+        id:1,
+        title:'Javascript',
+        link:'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+    },
+    {
+        id:2,
+        title:'Reactjs',
+        link:'https://react.dev/'
+    },
+    {
+        id:3,
+        title:'Nextjs',
+        link:'https://nextjs.org/'
+    },
+    {
+        id:4,
+        title:'Nodejs',
+        link:'https://nodejs.org/en'
+    },
+    {
+        id:5,
+        title:'Typescript',
+        link:'https://www.typescriptlang.org/'
+    },
+    {
+        id:6,
+        title:'Expressjs',
+        link:'https://expressjs.com/'
+    },
+    {
+        id:7,
+        title:'Reactjs',
+        link:'https://react.dev/'
+    },
+    {
+        id:8,
+        title:'Redux Toolkit',
+        link:'https://www.mongodb.com/'
+    },
+    {
+        id:9,
+        title:'Google Firebase',
+        link:'https://firebase.google.com/'
+    },
+    {
+        id:10,
+        title:'Tailwindcss',
+        link:'https://tailwindcss.com/'
+    },
+    {
+        id:11,
+        title:'MongoDB',
+        link:'https://www.mongodb.com/'
+    },
+    {
+        id:12,
+        title:'Sanity.io',
+        link:'https://www.sanity.io/'
+    },
+    {
+        id:13,
+        title:'HTML5',
+        link:'https://developer.mozilla.org/en-US/docs/Glossary/HTML5'
+    },
+    {
+        id:14,
+        title:'CSS3',
+        link:'https://developer.mozilla.org/en-US/docs/Web/CSS'
+    },
+    {
+        id:15,
+        title:'VS Code',
+        link:'https://code.visualstudio.com/'
+    },
+    {
+        id:16,
+        title:'Atom',
+        link:'https://github.blog/2022-06-08-sunsetting-atom/'
+    },
+    {
+        id:17,
+        title:'Git',
+        link:'https://git-scm.com/'
+    },
+    {
+        id:18,
+        title:'Github',
+        link:'https://github.com/'
+    },
+    {
+        id:19,
+        title:'Trello',
+        link:'https://trello.com/en'
+    },
+    {
+        id:20,
+        title:'GitKraken',
+        link:'https://www.gitkraken.com/'
+    },
+    {
+        id:21,
+        title:'UI Design',
+        link:'https://www.figma.com/'
+    },
+    {
+        id:22,
+        title:'Figma',
+        link:'https://www.figma.com/'
+    },
+    {
+        id:23,
+        title:'Canva',
+        link:'https://www.canva.com/'
+    },
+    {
+        id:24,
+        title:'UI Design',
+        link:'https://www.figma.com/'
+    },
+    {
+        id:25,
+        title:'Adobe Illustrator',
+        link:'https://www.adobe.com/products/illustrator/free-trial-download.html'
+    },
+    {
+        id:26,
+        title:'Brand & LogoDesign',
+        link:'https://www.figma.com/'
+    },
+    {
+        id:27,
+        title:'Website Design',
+        link:'https://themeforest.net/'
+    },
+    {
+        id:28,
+        title:'Vercel',
+        link:'https://vercel.com/'
+    },
+    {
+        id:29,
+        title:'netlify',
+        link:'https://www.netlify.com/'
+    },
+]
