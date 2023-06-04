0db55bb448b643e825c920dfafec5f10f373be2f
Iman Saadat <e.saadat72@gmail.com>
2023-06-04 23:32:37 +0330

    ExperienceCard was updated

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -6,9 +6,7 @@ interface Item {
   icon: JSX.Element;
 }
 
-const ExperienceCard: React.FC<{ item: Item }> = ({ item }) => {
-  const { title, subTitle, icon } = item;
-
+const ExperienceCard: React.FC<{ item: Item }> = ({ item: { title, subTitle, icon } }) => {
   return (
     <div className="flex items-center gap-4">
       <span className="w-12 h-12 rounded-full bg-black border-[1px] border-blue-600 flex items-center justify-center">
