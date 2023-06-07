bb7860b823c00a5890ba5c716f5c8215fddf1821
Iman Saadat <e.saadat72@gmail.com>
2023-06-08 00:19:35 +0330

    Footer was updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <p className="text-sm py-2">© 2022 reactBD All rights reserved.</p>;
+  return <p className="text-sm py-2 text-center">© 2022 reactBD All rights reserved.</p>;
 };
 
 export default Footer;
